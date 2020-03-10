71962a7ed4be346bc9c8bb80be6126c12a4d3748
Luan Oliveira <luanecomp@yahoo.com.br>
2020-03-09 21:03:48 -0300

    Arquivo settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': os.environ.get('DB_NAME', 'mysite_py'),
@@ -127,3 +127,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
